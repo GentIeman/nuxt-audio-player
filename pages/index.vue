1208594c705478c1bb92fdde96cc10730b655287
GentIeman <76070836+GentIeman@users.noreply.github.com>
2021-07-06 20:58:36 +0300

    add base for the player

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,14 @@
 <template>
+  <section class="page">
+    <section class="base">
+      <transition name="animate-slide-up">
+        <div class="base_circle circle"></div>
+      </transition>
+      <transition name="animate-slide-down">
+        <div class="base_circle circle"></div>
+      </transition>
+    </section>
+  </section>
 </template>
 
 <script>
@@ -7,4 +17,67 @@ export default {
 </script>
 
 <style lang="stylus">
+*,
+*:before,
+*:after {
+  margin 0
+  padding 0
+}
+
+.page {
+  display flex
+  justify-content center
+  align-items center
+  width 100vw
+  height 100vh
+
+  .base {
+    position relative
+    width 700px
+    height 400px
+    border-radius 18px
+    border solid 1px #B7B3B3
+    overflow hidden
+
+    &:before {
+      content ''
+      position absolute
+      top 0
+      left 0
+      width 110%
+      height 110%
+      background rgba(255, 255, 255, 0.2)
+      border-radius 18px
+      backdrop-filter blur(5px)
+      z-index 1
+    }
+
+    &__circle {
+      position relative
+    }
+
+    .circle {
+      position absolute
+      width 350px
+      height 350px
+      border-radius 100%
+      background-color #2ED573
+
+      &:first-child {
+        width 450px
+        height 450px
+        bottom 0
+        left 0
+        transform translate(-40%, 30%)
+      }
+
+      &:last-child {
+        top 0
+        right 0
+        transform translate(30%, -30%)
+      }
+    }
+  }
+}
+
 </style>
