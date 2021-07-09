be770731633457a773226495876e01edfdf69eda
GentIeman <76070836+GentIeman@users.noreply.github.com>
2021-07-09 03:10:53 +0300

    minor changes

--- components/v-control-panel.vue
+++ components/v-control-panel.vue
@@ -1,24 +1,24 @@
 <template>
   <section class="panel">
     <div class="panel__shuffle">
-      <img src="@/static/icons/shuffle.svg" alt="shuffle-icon">
+      <img src="@/static/icons/shuffle.svg" alt="shuffle-icon" width="30px">
     </div>
     <div class="main-btns">
       <div class="main-btns__previous-song">
-        <img src="@/static/icons/previous-song.svg" alt="previous-song">
+        <img src="@/static/icons/previous-song.svg" alt="previous-song" width="30px">
       </div>
       <div class="main-btns__play-song" v-if="isPlayed === true" @click="played">
-        <img src="@/static/icons/play.svg" alt="play-btn">
+        <img src="@/static/icons/play.svg" alt="play-btn" width="40px">
       </div>
       <div class="main-btns__pause-song" v-else @click="stop">
-        <img src="@/static/icons/pause.svg" alt="pause-btn">
+        <img src="@/static/icons/pause.svg" alt="pause-btn" width="40px">
       </div>
       <div class="main-btns__next-song">
-        <img src="@/static/icons/next-song.svg" alt="next-song">
+        <img src="@/static/icons/next-song.svg" alt="next-song" width="30px">
       </div>
     </div>
     <div class="panel__repeat">
-      <img src="@/static/icons/repeat.svg" alt="repeat">
+      <img src="@/static/icons/repeat.svg" alt="repeat" width="30px">
     </div>
   </section>
 </template>
@@ -27,13 +27,15 @@
 export default {
   name: "v-control-panel",
   data: () => ({
-    isPlayed: true
+    isPlayed: true // Music playback and background animation
   }),
   methods: {
+    // Start animation background
     played() {
       this.isPlayed = false
       this.$emit('playAnimation')
     },
+    // Stop animation background
     stop() {
       this.isPlayed = true
       this.$emit('stopAnimation')
@@ -51,7 +53,7 @@ export default {
   bottom 0
   left 50%
   transform translate(-50%, -40%)
-  width 400px
+  width 350px
   z-index 2
 
   & > div {
