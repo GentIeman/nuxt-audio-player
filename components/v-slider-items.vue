60ce58ec40f19e47126effb2f9e1183238bb42d7
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2021-07-30 22:22:26 +0300

    new styles for slides

--- components/v-slider-items.vue
+++ components/v-slider-items.vue
@@ -12,67 +12,48 @@ export default {
 </script>
 
 <style scoped lang="stylus">
-.slider__body {
-  display flex
-  justify-content center
-  align-items center
+.slider__slide {
+  position relative
+  border-radius 18px
+  width 150px
+  height 150px
+  overflow hidden
+  transition all .5s
 
-  .slider__slide {
-    position relative
-    border-radius 18px
-    width 150px
-    height 150px
-    overflow hidden
-    transition all .5s
-
-    .album {
-      position absolute
-      top 0
-      left 0
-      width 100%
-      height 100%
-
-      &:after {
-        content ''
-        position absolute
-        top: 0
-        left: 0
-        width 100%
-        height 100%
-        background-color rgba(0, 0, 0, 0.8)
-      }
-    }
-
-
-    &:first-child,
-    &:last-child {
-
-      &:after {
-        content ''
-        position absolute
-        top: 0
-        left: 0
-        width 100%
-        height 100%
-        background-color rgba(0, 0, 0, 0.8)
-      }
-    }
-
-    &:first-child {
-      position relative
-      left 40px
-    }
-
-    &:last-child {
-      position relative
-      right 40px
-    }
+  &:first-child {
+    left 50px
   }
 
-  .active-slide {
-    width 200px
-    height 200px
-    z-index 3
+  &:last-child {
+    right 50px
+  }
+
+  .album {
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+  }
+
+  &:after {
+    content ''
+    position absolute
+    top 0
+    left 0
+    width 100%
+    height 100%
+    background-color rgba(0, 0, 0, 0.6)
+  }
+}
+
+.slider__slide_active {
+  width 200px
+  height 200px
+  z-index 3
+
+  &:after{
+    display none
   }
 }
 </style>
