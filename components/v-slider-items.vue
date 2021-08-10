e4942ca8290072cfc6ed104a51d6c173474b6df9
Ilya Shepelev <76070836+GentIeman@users.noreply.github.com>
2021-08-10 03:06:19 +0300

    fixed styles of slider elements

--- components/v-slider-items.vue
+++ components/v-slider-items.vue
@@ -1,24 +1,23 @@
 <template>
-  <div class="slider__slide" :class="{'slider__slide_active': active}">
+  <div class="slider__slide">
     <img class="album" :src="`/albums/${data.album}.jpg`" alt="album">
   </div>
 </template>
 
 <script>
 export default {
-  props: ['data', 'active'],
+  props: ['data'],
   name: "v-slider"
 }
 </script>
 
 <style scoped lang="stylus">
 .slider__slide {
-  position absolute
+  position relative
   border-radius 18px
-  width 150px
-  height 150px
+  width 200px
+  height 200px
   overflow hidden
-  transition all .5s
 
   .album {
     position absolute
@@ -26,27 +25,6 @@ export default {
     left 0
     width 100%
     height 100%
-  }
-
-  &:after {
-    content ''
-    position absolute
-    top 0
-    left 0
-    width 100%
-    height 100%
-    background-color rgba(0, 0, 0, 0.6)
-  }
-}
-
-.slider__slide_active {
-  position relative
-  width 200px
-  height 200px
-  z-index 3
-
-  &:after {
-    display none
   }
 }
 
